7fdf6bef76f2e76627cf5353dbc23b1a03637b48
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2024-10-25 22:00:55 +0600

    Added DRF,Serializer

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,18 @@
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
 from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'index.html', {})
+# def index(request):
+#     return render(request, 'index.html', {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
